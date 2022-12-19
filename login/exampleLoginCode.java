6fbb574090ff9b8bec70c93a2f35ad5b5b31f8db
Rob <69918580+bruhmoent@users.noreply.github.com>
2022-12-19 12:12:17 +0100

    Update exampleLoginCode.java

--- login/exampleLoginCode.java
+++ login/exampleLoginCode.java
@@ -1,19 +1,24 @@
-//Example code how to handle SQL requests in eclipse with Jframe (the code should be put into a button)	
+//You also need the mysql-connector-java-(ver).jar file to perform these requests
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.awt.EventQueue;
+import com.mysql.cj.xdevapi.Statement;
 public void actionPerformed(ActionEvent e) {
+	//Example code how to handle SQL requests in eclipse with Jframe (the code should be put inside of a button's action handler)	
 				try
-				{
-					
+				{	
 				Class.forName("com.mysql.cj.jdbc.Driver");
-		
 				Connection con = DriverManager.getConnection("jdbc:mysql://yourlocalhost/database", "myusername", "mypassword");
 				java.sql.Statement stm=con.createStatement();
-				String sql = "Select * from tblogin WHERE username='"+yourInputField.getText()+"' AND password = '"+YourPassWord.getText()+"';";
+				String sql = "Select * from yourTableName WHERE field1='"+yourInputField.getText()+"' AND field2 = '"+yourInputField2.getText()+"';";
 				ResultSet rs = stm.executeQuery(sql);
+					
 				if(rs.next())
 				{
 					JOptionPane.showMessageDialog(null, "Your Message");
 				}
-				else {
+				else 
+				{
 					JOptionPane.showMessageDialog(null, "Your Message");
 					con.close();
 				}
